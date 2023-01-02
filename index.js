0f5f2f0cf747133a18920cfafe1ccde27c2fe894
Rayenbt11 <2021378@student.cct.ie>
2023-01-02 00:41:35 +0000

    add comments

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ const   http = require('http'), //HTTP server
         xml2js = require('xml2js'),
         server = http.createServer(router); //Init our server
         
+        // Serve static files and parse request bodies
         router.use(express.static(path.resolve(__dirname,'views')));
         router.use(express.urlencoded({extended: true}));
         router.use(express.json());
 
+// Converts an XML file to a JSON object
 function XMLtoJSON(filename, cb){
     let filepath = path.normalize(path.join(__dirname, filename));
     fs.readFile(filepath, 'utf8', function(err, xmlStr){
-        if (err) throw (err);
+        if (err) throw (err);               
         xml2js.parseString(xmlStr, {}, cb);
     });
 };
 
+// Converts a JSON object to an XML file
 function JSONtoXML(filename, obj, cb){
     let filepath = path.normalize(path.join(__dirname, filename));
     let builder = new xml2js.Builder();
@@ -28,39 +31,48 @@ function JSONtoXML(filename, obj, cb){
     fs.writeFile(filepath, xml, cb);
 };
 
+// GET route for serving an HTML page generated from an XML and XSL file
 router.get('/get/html', function(req, res) {
 
+        // Set response type to HTML
     res.writeHead(200, {'Content-Type' : 'text/html'});
-
+    
+    // Read XML and XSL files
     let xml = fs.readFileSync('list.xml', 'utf8'),
         xsl = fs.readFileSync('list.xsl', 'utf8');
 
     xml = xmlParse(xml);
-    xsl = xmlParse(xsl);
-
+    xsl = xmlParse(xsl);   
+    
+    // Apply XSL transformation to XML and convert to string
     let html = xsltProcess(xml, xsl);
-
+    // Send HTML response
     res.end(html.toString());
 });
+
+// POST route for appending data to an XML file in JSON format
 
 router.post('/post/json', function(req, res){
     function appendJSON(obj){
         console.log(obj);
+        // Read XML file and convert to JSON
         XMLtoJSON('list.xml', function(err, result) {
             if (err) throw (err);
             result.list.priority[obj.sec_n].task.push({'description': obj.description, 'Due': obj.Due});
             console.log(JSON.stringify(result, null, " "));
+                    // Convert modified JSON object back to XML and write to file
             JSONtoXML('list.xml', result, function(err){
                 if (err) console.log(err);
             });
         });
     };
-
+// Read request body and append to XML file
     appendJSON(req.body);
-
+// Redirect back to previous page
     res.redirect('back');
 });
 
+// POST route for deleting an element from an XML file
 router.post('/post/delete', function (req,res) {
     function deleteJSON(obj) {
         console.log(obj);
@@ -79,7 +91,7 @@ router.post('/post/delete', function (req,res) {
 
     res.redirect('back');
 })
-
+// Start HTTP server and listen for requests
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
     console.log("Server listening at", addr.address + ":" + addr.port)
--- views/js/table.js
+++ views/js/table.js
@@ -1,32 +1,52 @@
-function draw_table(){
+// This function retrieves an HTML table from the server and appends it to the element 'results'
+function draw_table() {
     $("#results").empty();
-    $.getJSONuncached = function(url){
+    // Define a function that retrieves a JSON object from the specified URL
+    // The function uses AJAX to send a GET request to the URL, and appends the returned HTML to the element 'results'
+    // The cache is disabled to ensure that the most up-to-date data is retrieved
+    $.getJSONuncached = function (url) {
         return $.ajax(
-        {
-            url: url,
-            type: 'GET',
-            cache: false,
-            success: function(html){
-                $("#results").append(html);
-                select_row();
-            }
-        });
+            {
+                url: url,
+                type: 'GET',
+                cache: false,
+                success: function (html) {
+                    $("#results").append(html);
+                    // Once the HTML is appended, call the 'select_row' function
+                    select_row();
+                }
+            });
     };
+    // Call the function defined above, with the URL '/get/html'
     $.getJSONuncached("/get/html")
 };
 
-function select_row(){
-    $("#todoTable tbody tr[id]").click(function(){
+// This function adds a click event listener to each row in the table with ID 'todoTable'
+// When a row is clicked, the 'selected' class is added to it, and removed from all other rows
+// The function also calls the 'delete_row' function, passing the section and entree indices of the clicked row
+function select_row() {
+    $("#todoTable tbody tr[id]").click(function () {
+        // Remove the 'selected' class from all rows
         $(".selected").removeClass("selected");
+        // Add the 'selected' class to the clicked row
+
         $(this).addClass("selected");
+        // Calculate the section index of the clicked row
+
         var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
+        // Calculate the entree index of the clicked row
+
         var entree = $(this).attr("id") - 1;
+        // Call the 'delete_row' function, passing the calculated section and entree indices
+
         delete_row(section, entree);
     });
 };
-
-function delete_row(sec, ent){
-    $("#delete").click(function (){
+// This function adds a click event listener to the element with ID 'delete'
+// When the element is clicked, the function sends a POST request to the server, with the specified section and entree indices
+// The function also calls the 'draw_table' function, with a delay of 1000 milliseconds (1 second)
+function delete_row(sec, ent) {
+    $("#delete").click(function () {
         $.ajax({
             url: "/post/delete",
             type: "POST",
@@ -40,6 +60,7 @@ function delete_row(sec, ent){
     });
 };
 
-$(document).ready(function(){
+// When the document is ready, call the 'draw_table' function
+$(document).ready(function () {
     draw_table();
 });
